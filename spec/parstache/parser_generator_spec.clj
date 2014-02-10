eb7e8169abffa117d59f1b8386739478e51193cf
Wai Lee Chin Feman <skatenerd@gmail.com>
2014-02-09 20:04:47 -0600

    Add basic rules about generating a parse tree, ignores actual program content right now

--- spec/parstache/parser_generator_spec.clj
+++ spec/parstache/parser_generator_spec.clj
@@ -3,8 +3,8 @@
     [parstache.parser-generator :refer :all]
     [speclj.core :refer :all]))
 
-(describe "possible mutations"
-  (it "finds the obvious rule-add mutation"
+(describe "add to parse tree"
+  (it "finds all additions according to whatever the legally-addable-children function says to do"
     (let [intermediate-parse-tree {:type :root :children [{:type :child :children ["HOWDY!"]}]}
           root intermediate-parse-tree
           child (first (:children intermediate-parse-tree))
@@ -15,11 +15,78 @@
         possible-new-trees
         (add-to-tree intermediate-parse-tree legally-addable-children)))))
 
-;(describe "generate a parser"
-;  (it "parses aaa"
-;    (should=
-;      {:rule "REPEAT_A" :contents ["a" "a" "a"]}
-;      ((generate-parser
-;       [{:name "REPEAT_A" :type :string-repetition :value "a"}])
-;         "aaa"
-;         ))))
+(describe "what can i add to a node"
+  (it "knows about juxtaposition rules"
+    (let [rules {:root {:type :juxtaposition :children [:first :middle]}
+                 :first {:type :character :children ["a"]}
+                 :middle {:type :character :children ["z"]}}]
+      (should=
+        [{:name :middle :type :character :children []}]
+        (addable-children
+          rules
+          {:name :root :type :juxtaposition :children [{:name :first :type :character :children ["a"]}]}))))
+
+  (it "knows about character rules"
+    (let [rules {:root {:type :character :children ["a"]}}]
+      (should=
+        ["a"]
+        (addable-children
+          rules
+          {:name :root :type :character :children []}))))
+
+  (it "knows about repetition rules"
+    (let [rules {:root {:type :repetition :children [:char-rule]}
+                 :char-rule {:type :character :children ["Z"]}}]
+      (should=
+        [{:name :char-rule :type :character :children []}]
+        (addable-children
+          rules
+          {:name :root :type :repetition :children [{:name :char-rule :type :character :children ["Z"]}]}))))
+
+  (it "wont let you add anything to a node whose last child is not closable"
+    (let [rules {:root {:type :repetition :children [:char-rule]}
+                 :char-rule {:type :character :children ["Z"]}}]
+      (should=
+        []
+        (addable-children
+          rules
+          {:name :root :type :repetition :children [{:name :char-rule :type :character :children []}]})))
+    )
+
+
+  )
+
+(describe "closable?"
+  (it "A juxtaposition is not closeable unless its stuff is all finished"
+    (should (closeable?
+              {:root {:type :juxtaposition :children [:char-rule]}
+               :char-rule {:type :character :children ["Z"]}}
+              {:name :root :type :juxtaposition :children [{:name :char-rule :type :character :children ["Z"]}]}
+              ))
+    (should-not (closeable?
+                  {:root {:type :juxtaposition :children [:char-rule]}
+                   :char-rule {:type :character :children ["Z"]}}
+                  {:name :root :type :juxtaposition :children []})))
+  (it "a character rule is not closeable unless it has a child"
+    (should
+      (closeable?
+        {:root {:type :character :children ["A"]}}
+        {:name :root :type :character :children ["A"]}))
+    (should-not
+      (closeable?
+        {:root {:type :character :children ["A"]}}
+        {:name :root :type :character :children []})))
+  (it "a repetition rule is always closeable"
+    (should
+      (closeable?
+        {:root {:type :repetition :children [:wat]}}
+        {:name :root :type :repetiition :children []})))
+  (it "nil is always closeable"
+    (should
+      (closeable? {} nil)))
+  (it "a node is not closeable unless its last child is also closeable"
+    (should-not
+      (closeable?
+        {:root {:type :juxtaposition :children [:char-rule]}
+         :char-rule {:type :character :children ["Z"]}}
+        {:name :root :type :juxtaposition :children [{:name :char-rule :type :character :children []}]}))))
--- src/parstache/parser_generator.clj
+++ src/parstache/parser_generator.clj
@@ -1,7 +1,7 @@
 (ns parstache.parser-generator
   (:require [parstache.tree :refer :all]))
 
-(declare add-to-tree)
+(declare add-to-tree closeable?)
 
 (defn new-last-element [v new-element]
   (assoc-in v [(dec (count v))] new-element))
@@ -37,16 +37,56 @@
         (with-altered-subtree intermediate-parse-tree legally-addable-children)]
     (concat with-altered-subtree with-immediate-adds)))
 
+(defn rules-with-metadata [rules]
+  (into {} (map (fn [[key val]] [key (assoc val :name key)]) rules)))
 
-;(defn generate-parser [rules]
-;  (fn [to-parse]
-;    {:rule "REPEAT_A" :contents ["a" "a" "a"]}))
+(defn build-empty-node [rule-name rules]
+  (assoc (get rules rule-name) :children []))
 
-;closeable?
-;if nil, true
-;else,
-;last child is closeable, and own constraints resolved
-;
-;add a rule, or add a token
-;
-;jkjk
+(defn addable-to-juxtaposition [rules rule node]
+  (let [wants (get rule :children)
+        has (get node :children)
+        next-up (nth wants (count has))]
+    [(build-empty-node next-up rules)]))
+
+(defn addable-to-character [rules rule node]
+  (let [wants (get rule :children)]
+    wants))
+
+(defn addable-to-repetition [rules rule node]
+  (let [wants (get rule :children)]
+    [(build-empty-node (first wants) rules)]))
+
+(defn addable-children [rules node]
+  (let [rules (rules-with-metadata rules)
+        rule (get rules (:name node))
+        last-child-closeable? (closeable? rules (last (:children node)))]
+    (if last-child-closeable?
+      (case (:type rule)
+        :juxtaposition
+        (addable-to-juxtaposition rules rule node)
+        :character
+        (addable-to-character rules rule node)
+        :repetition
+        (addable-to-repetition rules rule node))
+      [])))
+
+(defn juxtaposition-closeable? [rule node]
+  (let [child-names (map :name (:children node))]
+    (= child-names (:children rule))))
+
+(defn character-closeable? [rule node]
+  (not (empty? (:children node))))
+
+(defn closeable? [rules node]
+  (if (map? node)
+    (let [rule (get rules (:name node))
+          local-answer (case (:type rule)
+                         :juxtaposition
+                         (juxtaposition-closeable? rule node)
+                         :character
+                         (character-closeable? rule node)
+                         :repetition
+                         true)]
+      (and local-answer (recur rules (last (:children node)))))
+    true))
